72a13852dc80edaa344128229e70b4c146c5398e
MaxTinka <tinkamax03@gmail.com>
2025-02-15 10:14:29 +0300

    Update siteplan.js
    last

--- scripts/siteplan.js
+++ scripts/siteplan.js
@@ -1,11 +1,11 @@
 // Sample menu items with images
 const menuItems = [
     { name: "Margherita Pizza", price: 35000, image: "images/pizza2.jpg" },
-    { name: "Burgers", price: 21000, image: "images/bigburger.jpg" },
+    { name: "Tinka's king Burger", price: 21000, image: "images/bigburger.jpg" },
     { name: "Chicken Nuggets", price: 15000, image: "images/nuggets2.jpg" },
     { name: "Chocolate Cake", price: 8000, image: "images/cake2.jpg" },
-    { name: "Vanilla Ice Cream", price: 10000, image: "images/icecream2.jpg" },
-    { name: "Fried Chicken", price: 18000, image: "images/chicken2.jpg" },
+    { name: "Ice Creams", price: 10000, image: "images/icecream2.jpg" },
+    { name: "Crispy Chicken", price: 18000, image: "images/chicken2.jpg" },
     { name: "Tomato Soup", price: 15000, image: "images/soup1.jpg" },
     { name: "Coffee", price: 10000, image: "images/coffee2.jpg" },
 ];
